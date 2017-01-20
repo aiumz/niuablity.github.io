ce50a2ca7d0ac537e371850652a220d0fd0680ee
nmzbusiness <352784769@qq.com>
2017-01-20 17:18:13 +0800

    config

---
+++ js/index.js
@@ -0,0 +1,49 @@
+var config={
+	"website":[
+		{
+			"name":"github",
+			"src":"https://github.com/"
+		},		
+		{
+			"name":"bootcdn",
+			"src":"http://www.bootcdn.cn/"
+		},		
+		{
+			"name":"bootstrap",
+			"src":"http://www.bootcss.com/"
+		},		
+		{
+			"name":"jQuery API",
+			"src":"http://jquery.cuishifeng.cn/"
+		},		
+		{
+			"name":"廖雪峰的官网",
+			"src":"http://www.liaoxuefeng.com/wiki/0013739516305929606dd18361248578c67b8067c8c017b000"
+		}
+	],
+	"angular":[],
+	"node":[],
+	"mongodb":[],
+	"niuchat":[],
+	"other":[],
+	"beyondcode":[
+		{
+			"name":"thinking",
+			"src":"http://jquery.cuishifeng.cn/"
+		}
+	]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
